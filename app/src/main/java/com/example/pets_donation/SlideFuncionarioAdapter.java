5a29cc77e7630846dd2221b35c8cbdf19f918a90
Nikolas <nikolas.genesio@estudante.ufjf.br>
2022-08-01 09:48:12 -0300

    Atualizações nas interfaces

---
+++ app/src/main/java/com/example/pets_donation/SlideFuncionarioAdapter.java
@@ -0,0 +1,69 @@
+package com.example.pets_donation;
+
+import android.content.Context;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.RelativeLayout;
+import android.widget.TextView;
+
+import org.w3c.dom.Text;
+
+import androidx.annotation.NonNull;
+import androidx.viewpager.widget.PagerAdapter;
+
+public class SlideFuncionarioAdapter extends PagerAdapter {
+
+    Context context;
+    LayoutInflater layoutInflater;
+
+
+    public SlideFuncionarioAdapter(Context context) {
+        this.context = context;
+    }
+
+    public int[] slide_images = {
+            R.drawable.funcionario1,
+            R.drawable.funcionario4,
+            R.drawable.funcionario2,
+            R.drawable.funcionario3
+    };
+
+    public String[] slide_headings = {
+            "dica1", "dica2", "dica3", "dica4"
+    };
+
+
+    @Override
+    public int getCount() {
+        return slide_headings.length;
+    }
+
+    @Override
+    public Object instantiateItem(ViewGroup container, int position) {
+
+        layoutInflater = (LayoutInflater) context.getSystemService(context.LAYOUT_INFLATER_SERVICE);
+        View view = layoutInflater.inflate(R.layout.slide_layout, container, false);
+
+        ImageView slideImagemView = (ImageView) view.findViewById(R.id.imageView2);
+
+        slideImagemView.setImageResource(slide_images[position]);
+
+        container.addView(view);
+        return view;
+
+    }
+
+
+    @Override
+    public void destroyItem(ViewGroup container, int position, Object object) {
+        container.removeView((RelativeLayout) object);
+    }
+
+    @Override
+    public boolean isViewFromObject(@NonNull View view, @NonNull Object object) {
+        return view == (RelativeLayout) object;
+    }
+
+}
--- app/src/main/java/com/example/pets_donation/ui/fragments/FuncionarioInicio_Fragment.java
+++ app/src/main/java/com/example/pets_donation/ui/fragments/FuncionarioInicio_Fragment.java
@@ -2,20 +2,37 @@ package com.example.pets_donation.ui.fragments;
 
 import android.os.Bundle;
 
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.fragment.app.Fragment;
+import androidx.viewpager.widget.ViewPager;
 
+import android.text.Html;
 import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.LinearLayout;
+import android.widget.TextView;
 
 import com.example.pets_donation.Funcionario;
 import com.example.pets_donation.R;
+import com.example.pets_donation.SlideFuncionarioAdapter;
 
 public class FuncionarioInicio_Fragment extends Fragment {
 
+    private ViewPager mSlideViewPager;
+    private TextView textView;
+    private LinearLayout mDotLayout;
+    private SlideFuncionarioAdapter slideAdapter;
+    private TextView[] mDots;
+    private Button mNextBtn;
+    private Button mBackBtn;
+    private int mCurrentPage;
     private Funcionario funcionario;
+
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
@@ -25,4 +42,84 @@ public class FuncionarioInicio_Fragment extends Fragment {
         Log.i("Funcionario Inicio", "NOME: " + funcionario.getNome());
         return inflater.inflate(R.layout.fragment_funcionario_inicio_, container, false);
     }
+
+    public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
+        mSlideViewPager = (ViewPager) view.findViewById(R.id.view);
+        mDotLayout = (LinearLayout) view.findViewById(R.id.linear);
+        mNextBtn = (Button) view.findViewById(R.id.btnContinuar1);
+        mBackBtn = (Button) view.findViewById(R.id.btnVoltar1);
+        textView = (TextView) view.findViewById(R.id.funcao);
+        slideAdapter = new SlideFuncionarioAdapter(getActivity());
+        mSlideViewPager.setAdapter(slideAdapter);
+        textView.setText("Cadastrar Abrigos!");
+
+        addDotsIndicator(0);
+
+        mSlideViewPager.addOnPageChangeListener(viewListener);
+    }
+
+    public void addDotsIndicator(int position) {
+        mDots = new TextView[4];
+        mDotLayout.removeAllViews();
+
+        for (int i = 0; i < mDots.length; i++) {
+
+            mDots[i] = new TextView(getActivity());
+            mDots[i].setText(Html.fromHtml("&#8226;"));
+            mDots[i].setTextSize(35);
+            mDots[i].setTextColor(getResources().getColor(R.color.laranja));
+
+            mDotLayout.addView(mDots[i]);
+        }
+        if (mDots.length > 0) {
+            mDots[position].setTextColor(getResources().getColor(R.color.colorHint));
+        }
+    }
+
+    ViewPager.OnPageChangeListener viewListener = new ViewPager.OnPageChangeListener() {
+        @Override
+        public void onPageScrolled(int i, float v, int i1) {
+
+        }
+
+        @Override
+        public void onPageSelected(int i) {
+            addDotsIndicator(i);
+
+            mCurrentPage = i;
+
+            if (i == 0) {
+                mNextBtn.setEnabled(true);
+                mBackBtn.setEnabled(false);
+                mBackBtn.setVisibility(View.INVISIBLE);
+
+                textView.setText("Cadastrar Abrigos!");
+                mNextBtn.setText("");
+                mBackBtn.setText("");
+            } else if (i == 1) {
+                textView.setText("Cadastrar Animais!");
+            } else if (i == mDots.length - 1) {
+                mNextBtn.setEnabled(true);
+                mBackBtn.setEnabled(true);
+                mBackBtn.setVisibility(View.INVISIBLE);
+                textView.setText("Realizar adoção!");
+                mNextBtn.setText("");
+                mBackBtn.setText("");
+            } else {
+                mNextBtn.setEnabled(true);
+                mBackBtn.setEnabled(true);
+                mBackBtn.setVisibility(View.INVISIBLE);
+                textView.setText("Gerar relatórios!");
+                mNextBtn.setText("");
+                mBackBtn.setText("");
+            }
+
+        }
+
+
+        @Override
+        public void onPageScrollStateChanged(int i) {
+
+        }
+    };
 }
